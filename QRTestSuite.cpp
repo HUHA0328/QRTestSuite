0d80a1fa3e8374ed0f88eed918653dc01006737a
Fred <schreiber.frederik@gmail.com>
2017-10-21 20:56:11 +0200

    Multi QR Fixed Diagonal increment and decrement bug

--- QRTestSuite.cpp
+++ QRTestSuite.cpp
@@ -47,8 +47,8 @@ int fpsCalc(float fps);
 //int imageLoadTest();
 
 //Detector Methods
-vector<FiP> cv_FiPdetection(Mat inputImage, vector<FiP> prevImage);
-QRCode cv_QRdetection(vector<FiP> fipImage, QRCode qrpPrevImage);
+vector<vector<FiP>> cv_FiPdetection(Mat inputImage, vector<vector<FiP>> prevImage);
+QRCode cv_QRdetection(vector<FiP> fipImage, vector<QRCode> qrpPrevImage);
 int cv_HarrisCorner(Mat img);
 vector<FiP> cv_getFiPOrder(vector<FiP> unordered);
 
@@ -166,8 +166,8 @@ int QRTest()
 	int key = 0; //input key of the hughgui classbb
 	VideoCapture capture;//("C:/Users/Frederik/Desktop/VidTests/720-frame-jpg/image-%05d.jpg");
 	vector<int> timePassedAll;
-	vector<FiP> FiPList;
-	QRCode QRList = QRCode(0);
+	vector<vector<FiP>> FiPList;
+	vector<QRCode> QRList;
 	int solo = 0, partial = 0, full = 0, over = 0;
 	int frame = 0;
 	int decoded = 0;
@@ -192,7 +192,7 @@ int QRTest()
 		else if (benchmarktype == "jpg")
 			capture.open("C:/Users/Frederik/Desktop/VidTests/720-frame-jpg/image-%05d.jpg");
 		else if (benchmarktype == "png")
-			capture.open("C:/Users/Frederik/Desktop/VidTests/720-frame-png/image-%07d.png");
+			capture.open("C:/Users/Frederik/Desktop/VidTests/2FiPTestMulti.png");
 	}
 
 	if (!capture.isOpened()) {
@@ -249,13 +249,24 @@ int QRTest()
 		
 		
 		//Image Processing
-		FiPList = cv_FiPdetection(image, FiPList);
+		FiPList = cv_FiPdetection(image, FiPList); //<--- the QR list should go there
 
 		//cout << FiPList.size() << endl;
 
 		//grouping 
 
-		//QRList = cv_QRdetection(FiPList, QRList);
+		for (auto &fip : FiPList) {
+			QRCode qrNew = cv_QRdetection(fip, QRList);
+			if (qrNew.tag != 0) {
+				for (int i = 0; i < QRList.size(); i++) { //<- It should also be possible to optimize this but since we technicly should never get more than exsisting QR codes (so around 3) entrys it should always run very fast
+					if (QRList[i].tag == qrNew.tag) {
+						QRList.erase(QRList.begin() + i);
+						break;
+					}
+				}
+				QRList.push_back(qrNew);
+			} 
+		}
 		//decoded += QRList.decode_success;
 
 		
@@ -294,16 +305,16 @@ int QRTest()
 /* calling a method an reinitializing a lot of Variables should create a overhead compared to just use one method?
 	Maybe I should do a speed test since it would be far easier to understand in more methods but if it costs more time
 	nothing is really gained*/
-vector<FiP> cv_FiPdetection(Mat inputImage, vector<FiP> prevImage) /*
+vector<vector<FiP>> cv_FiPdetection(Mat inputImage, vector<vector<FiP>> prevImage ) /* //<--- the QR list should go there
 									input:
 
 									output:
 
 									*/
 {
-	vector<FiP> fipList;
+
+	vector<vector<FiP>> fipList;
 	vector<vector<Point> > contours, contoursMorph;
-	vector<vector<Point> > fiPReg;
 	vector<Vec4i> hierarchy, hierarchyMorph;
 	vector<Point> pointsseq;    //used to save the approximated sides of each contour
 	vector<vector<Point> > finderPatterns;
@@ -348,34 +359,29 @@ vector<FiP> cv_FiPdetection(Mat inputImage, vector<FiP> prevImage) /*
 	//cout << endl;
 	vector<Mat> image_roi;
 	int j = 0;
-	for (int i = 0; i < contoursMorph.size(); i += 2) {
-		approxPolyDP(contoursMorph[i], pointsseq, arcLength(contoursMorph[i], true)*0.05, true);
+	for (int i1 = 0; i1 < contoursMorph.size(); i1 += 2) {
+		approxPolyDP(contoursMorph[i1], pointsseq, arcLength(contoursMorph[i1], true)*0.05, true);
 		if (pointsseq.size() == 4 && isContourConvex(pointsseq)) {
-
+			vector<FiP> qrCandidate;
+			vector<vector<Point> > fiPReg;
+			//Maybe jump over obviously no QR-Codes ?
 			Rect regionOfInterest = cv_getRect(pointsseq[0], pointsseq[1], pointsseq[2], pointsseq[3], true);
 			image_roi.push_back(image(regionOfInterest));
-			//imshow("image", image_roi[j]);
 			cvtColor(image_roi[j], image_roi[j], CV_RGB2GRAY);
 			threshold(image_roi[j], image_roi[j], 180, 255, THRESH_BINARY);
 			copyMakeBorder(image_roi[j], image_roi[j], 5, 5, 5, 5, BORDER_CONSTANT, Scalar(255, 255, 255));
 			resize(image_roi[j], image_roi[j], Size(image_roi[j].size().width * 2, image_roi[j].size().height * 2));
 			Canny(image_roi[j], image_roi[j], 10, 200, 3);// 80, 150, 3);
 			findContours(image_roi[j], contours, hierarchy, RETR_TREE, CHAIN_APPROX_TC89_KCOS);
-			//imshow("CannyImage", image_roi[j]);
-			//imshow("image", image(regionOfInterest));
-			//waitKey(0);
-			//cvDestroyWindow("CannyImage");
-			//cvDestroyWindow("image");
 			j++;
-			//drawContours(image, vector<vector<Point> >(1, pointsseq), -1, Scalar(0, 0, 255), 2, 8);
 
-			for (int i = 0; i < contours.size(); i++)
+			for (int i2 = 0; i2 < contours.size(); i2++)
 			{
 				//Find the approximated polygon of the contour we are examining
-				approxPolyDP(contours[i], pointsseq, arcLength(contours[i], true)*0.02, true); // <- this somehow doesnt work
+				approxPolyDP(contours[i2], pointsseq, arcLength(contours[i2], true)*0.02, true); // <- this somehow doesnt work
 				if (pointsseq.size() == 4)      // only quadrilaterals contours are examined // <- this neither or I misunderstand it
 				{
-					int k = i;
+					int k = i2;
 					int c = 0;
 
 					while (hierarchy[k][2] != -1)
@@ -388,29 +394,57 @@ vector<FiP> cv_FiPdetection(Mat inputImage, vector<FiP> prevImage) /*
 					{
 						finderPatterns.push_back(contours[k]);
 						vector<Point> fipSquare;
-						approxPolyDP(contours[k - c], fipSquare, arcLength(contours[i], true)*0.02, true);
+						approxPolyDP(contours[k - c], fipSquare, arcLength(contours[i2], true)*0.02, true);
+						//fipSquare = contours[k - c];
+						for (int i3 = 0; i3 < fipSquare.size(); i3++) {
+							fipSquare[i3].x = fipSquare[i3].x/2 + regionOfInterest.x -5;
+							fipSquare[i3].y = fipSquare[i3].y/2 + regionOfInterest.y -5;
+						}
 						fiPReg.push_back(fipSquare);
+						if (c >= 5) {
+							i2 += 2;
+						}
 					} 
 					else
-						i += c;
+						i2 += c;
 				}
 			}
+			for (auto &fip : fiPReg) {
+				Point Center = cv_getCentroid(fip);
+				qrCandidate.push_back(FiP(Center, fip));
+			}
+			if (qrCandidate.size()>0)
+				fipList.push_back(qrCandidate);
 		}
+
 	}
 	
 
+	/*
+	for (int z = 0; z < fipList.size(); z++) {
+		Scalar groupCol;
+		if (z == 0)
+			groupCol = Scalar(255, 0, 0);
+		else if (z == 1) {
+			groupCol = Scalar(0, 255, 0);
+			waitKey(0);
+		}
+		else if (z == 2)
+			groupCol = Scalar(255, 255, 0);
+		else
+			groupCol = Scalar(0, 0, 255);
+		for (int zz = 0; zz < fipList[z].size(); zz++) {
+			drawContours(image, vector<vector<Point> >(1, fipList[z][zz].shape), -1, groupCol, 5, 8);
+		}
+	}*/
 	
 	
-	
-	for (auto &fip : fiPReg) {
-		Point Center = cv_getCentroid(fip);
-		fipList.push_back(FiP(Center, fip));
-	}
-	drawContours(image, fiPReg, -1, Scalar(0, 0, 255), 20, 8);
+
+	//drawContours(image, fiPReg, -1, Scalar(0, 0, 255), 1, 8);
 	return fipList;
 }
 
-QRCode cv_QRdetection(vector<FiP> fipImage, QRCode qrPrevImage) {
+QRCode cv_QRdetection(vector<FiP> fipImage, vector<QRCode> qrPrevImage) {
 	QRCode returnCode(0);
 	Point QRPos;
 	FiP fip_A, fip_B, fip_C;
@@ -418,11 +452,27 @@ QRCode cv_QRdetection(vector<FiP> fipImage, QRCode qrPrevImage) {
 	bool success = false;
 	String qrData = "";
 	int tag;
+	int oldQRindex;
 
 	// process FiPs
 	fipImage = cv_getFiPOrder(fipImage);
 	// if 3 or 2 diagonal we already have the position
 	//cout << fipImage.size() << endl;
+
+	//DEBUG DRAWING 
+	/*if (fipImage.size() == 2) {
+		Mat img2 = image;
+		for (auto &fip : fipImage) {
+			drawContours(img2, vector<vector<Point> >(1, fip.shape), -1, Scalar(0, 0, 255), 1, 8);
+		}
+		imshow("image", img2);
+		int key = 0;
+		key = waitKey(0);
+		if (key == 'd') {
+			cout << "wait" << endl;
+		}
+	}*/
+
 	if (fipImage.size() == 3) {
 		if (fipImage[0].relPos == 0) {
 			QRPos = (fipImage[1].pos + fipImage[2].pos)*.5;
@@ -456,7 +506,22 @@ QRCode cv_QRdetection(vector<FiP> fipImage, QRCode qrPrevImage) {
 			//Two parallel FiPs
 			fip_A = fipImage[0];
 			fip_B = fipImage[1];
-			QRPos = cv_find2FipPos(fip_A, fip_B, qrPrevImage);
+			QRCode oldQR = QRCode(0);
+			float mindist = 9999;
+			int i = 0;
+			for (auto &qr : qrPrevImage) {
+				float newDist = cv_euclideanDist(fip_A.pos, qr.pos);
+				if (newDist < mindist) {
+					QRPos = qr.pos;
+					mindist = newDist;
+					oldQR = qr;
+					oldQRindex = i;
+				}
+				i++;
+			}
+			QRPos = cv_find2FipPos(fip_A, fip_B, oldQR); // this doesn't work currently since it will not exclude other QRs in the image right now || it Should work once the code was properly found and there wasn't a big movement in between
+			//SO IT WILL BE OVERWRITTEN BY THE NOT FOUND CLAUSE 
+
 			//found = true;
 		}
 	}
@@ -467,7 +532,15 @@ QRCode cv_QRdetection(vector<FiP> fipImage, QRCode qrPrevImage) {
 		// ignore might work best? (only if this happens rarely which seems to be the case)
 		fip_A = fipImage[0];
 		//QRPos = cv_find1FiPPos(fip_A, qrPrevImage);
-		QRPos = qrPrevImage.pos;
+		float mindist = 9999;
+		for (auto &qr : qrPrevImage) {
+			float newDist = cv_euclideanDist(fip_A.pos, qr.pos);
+			if (newDist < mindist) {
+				QRPos = qr.pos;
+				mindist = newDist;
+			}
+		}
+		//QRPos = qrPrevImage.pos;
 	}
 
 	// check if tag exists else give new one
@@ -489,98 +562,132 @@ QRCode cv_QRdetection(vector<FiP> fipImage, QRCode qrPrevImage) {
 		//qrPrevImage.pos = QRPos;
 		//If none is found return a QRCode with tag 0
 		returnCode = QRCode(0);
-	} else if((cv_euclideanDist(qrPrevImage.pos, QRPos)) <= 150.0 && qrPrevImage.decode_success == true) {
-		// check if there is tag for the Pos
-		// if yes load data from app
-
-		//if (cv_vectorSize(qrPrevImage.pos - QRPos) <= 150.0) {
-		//cout << "direct" << endl;
-		qrPrevImage.pos = QRPos;
-		returnCode = qrPrevImage;
 	}
 	else {
-		// if no reconstruct planar QR code and load data
-		//Identify
-		
-		//get CornerPointA in Case it exists already
-		Point pA;
-		if (fip_A.relPos == -1)
-			pA = Point(0,0);
-		else 
-			pA = cv_getOuterCorner(fip_A, QRPos);
-		//get Corners A if it doesnt and D in any Case
-		Point pD;
-		int cornersOut = cv_findCorners(pA, fip_B, fip_C, pD, QRPos);
-		//cout << "Calculated Corners" << endl;
-		//Now with the corners Reconstruct the planar image of the QRCode
-
-		//TODO This only works with Point2f which isnt Paintable also our QR Image is quite Poor maybe we should get it from the original image 
-		//and not from the changed one to find the FIP -- but here we should see first what zBar or another library can make out of it 
-		vector<Point2f> qrImg, dst;
-		qrImg.push_back(Point2f(pA));
-		qrImg.push_back(Point2f(cv_getOuterCorner(fip_B,  QRPos)));
-		qrImg.push_back(Point2f(pD)); //<- order important?
-		qrImg.push_back(Point2f(cv_getOuterCorner(fip_C, QRPos)));
-
-		//For Testing Do different later!
-		Mat qr, qr_raw, qr_gray, qr_gray_sharp, qr_thres;
-		qr_raw = Mat::zeros(200, 200, CV_8UC3);
-		qr = Mat::zeros(200, 200, CV_8UC3);
-		qr_gray = Mat::zeros(200, 200, CV_8UC1);
-		qr_gray_sharp = Mat::zeros(200, 200, CV_8UC1);
-		qr_thres = Mat::zeros(200, 200, CV_8UC1);
-
-		//replace with non magic Numbers?
-		dst.push_back(Point2f(0, 0));
-		dst.push_back(Point2f(qr.cols, 0));
-		dst.push_back(Point2f(qr.cols, qr.rows));
-		dst.push_back(Point2f(0, qr.rows));
-
-		Mat warp_matrix;
-
-		if (qrImg.size() == 4 && dst.size() == 4)			// Failsafe for WarpMatrix Calculation to have only 4 Points with src and dst
-		{
-			warp_matrix = getPerspectiveTransform(qrImg, dst);
-			warpPerspective(image, qr_raw, warp_matrix, Size(200, 200));
-			copyMakeBorder(qr_raw, qr, 10, 10, 10, 10, BORDER_CONSTANT, Scalar(255, 255, 255));
-
-			cvtColor(qr, qr_gray, CV_RGB2GRAY);
-
-			//check image quality before processing?
-			threshold(qr_gray, qr_thres, 180, 255, CV_THRESH_BINARY);
-			imshow("QR code old", qr_thres);
-			//waitKey(0);
-			qrData = decode(qr_thres);
-			if (qrData != "ERROR")
-				success = true;
-			else
-				success = false;
+		QRCode oldQR = QRCode(0);
+		float mindist = 9999;
+		for (auto &qr : qrPrevImage) {
+			float newDist = cv_euclideanDist(QRPos, qr.pos);
+			if (newDist < mindist) {
+				mindist = newDist;
+				oldQR = qr;
+			}
 		}
+		if (mindist <= 150.0 && oldQR.decode_success == true) {
+			// check if there is tag for the Pos
+			// if yes load data from app
 
-		if (success) {
-			//If tag exists retag
-			tag = findTaginList(qrData);
-			if (tag == tagDataMap.size())
-				tagDataMap.push_back(qrData);
-			//int tag = tagnumber++; //<- usually find next free tag - global variable?
-
-			//create QRCode Object
-			QRCode newCode(tag);
-			newCode.pos = QRPos;
-			newCode.decode_success = success;
-			newCode.data = qrData; // <-- we need this or always over tag?
-
-			//if (fipImage.size() == 3) //<- only do this if we had all threeFiPs so its sure that pD and pA are right - not sure if this is the best idea anymore.
-			newCode.orientation = cv_getOrientation(pA, pD);
-
-			//here have a list that gets a new entry with the tag indentification pair
-
-			returnCode = newCode;
+			//if (cv_vectorSize(qrPrevImage.pos - QRPos) <= 150.0) {
+			//cout << "direct" << endl;
+			oldQR.pos = QRPos;
+			returnCode = oldQR;
 		}
 		else {
-			QRCode newCode(0);
-			newCode.pos = QRPos;
+			// if no reconstruct planar QR code and load data
+			//Identify
 
+			//get CornerPointA in Case it exists already
+			Point pA;
+			if (fip_A.relPos == -1)
+				pA = Point(0, 0);
+			else
+				pA = cv_getOuterCorner(fip_A, QRPos);
+			//get Corners A if it doesnt and D in any Case
+			Point pD;
+			int cornersOut = cv_findCorners(pA, fip_B, fip_C, pD, QRPos);
+			//cout << "Calculated Corners" << endl;
+			//Now with the corners Reconstruct the planar image of the QRCode
+
+			//TODO This only works with Point2f which isnt Paintable also our QR Image is quite Poor maybe we should get it from the original image 
+			//and not from the changed one to find the FIP -- but here we should see first what zBar or another library can make out of it 
+			vector<Point2f> qrImg, dst;
+			qrImg.push_back(Point2f(pA));
+			qrImg.push_back(Point2f(cv_getOuterCorner(fip_B, QRPos)));
+			qrImg.push_back(Point2f(pD)); //<- order important?
+			qrImg.push_back(Point2f(cv_getOuterCorner(fip_C, QRPos)));
+
+			//for drawining debug version
+			vector<Point> qrImg2;
+			qrImg2.push_back(Point(pA));
+			qrImg2.push_back(Point(cv_getOuterCorner(fip_B, QRPos)));
+			qrImg2.push_back(Point(pD)); //<- order important?
+			qrImg2.push_back(Point(cv_getOuterCorner(fip_C, QRPos)));
+
+			//For Testing Do different later!
+			Mat qr, qr_raw, qr_gray, qr_gray_sharp, qr_thres;
+			qr_raw = Mat::zeros(200, 200, CV_8UC3);
+			qr = Mat::zeros(200, 200, CV_8UC3);
+			qr_gray = Mat::zeros(200, 200, CV_8UC1);
+			qr_gray_sharp = Mat::zeros(200, 200, CV_8UC1);
+			qr_thres = Mat::zeros(200, 200, CV_8UC1);
+
+			//replace with non magic Numbers?
+			dst.push_back(Point2f(0, 0));
+			dst.push_back(Point2f(qr.cols, 0));
+			dst.push_back(Point2f(qr.cols, qr.rows));
+			dst.push_back(Point2f(0, qr.rows));
+
+			Mat warp_matrix;
+
+			if (qrImg.size() == 4 && dst.size() == 4)			// Failsafe for WarpMatrix Calculation to have only 4 Points with src and dst
+			{
+				warp_matrix = getPerspectiveTransform(qrImg, dst);
+				//warpPerspective(image, qr_raw, warp_matrix, Size(200, 200));
+				warpPerspective(image, qr_raw, warp_matrix, Size(200, 200)); // we could get this from the already worked on image!
+				copyMakeBorder(qr_raw, qr, 10, 10, 10, 10, BORDER_CONSTANT, Scalar(255, 255, 255));
+
+				cvtColor(qr, qr_gray, CV_RGB2GRAY); //could save this
+
+				//check image quality before processing?
+				threshold(qr_gray, qr_thres, 180, 255, CV_THRESH_BINARY); //and this
+
+				qrData = decode(qr_thres);
+				cout << qrData << endl;
+				if (qrData != "ERROR")
+					success = true;
+				else
+					success = false;
+
+				if (qrData == "ERROR") {
+					// FOR DEBUG DRAWINGS
+					drawContours(image, vector<vector<Point> >(1, qrImg2), -1, Scalar(0, 0, 255), 1, 8);
+					circle(image, qrImg2[0], 10, Scalar(0, 255, 0), -1, 8, 0);
+					circle(image, qrImg2[1], 8, Scalar(255, 255, 0), -1, 8, 0);
+					circle(image, qrImg2[2], 6, Scalar(0, 255, 255), -1, 8, 0);
+					circle(image, qrImg2[3], 4, Scalar(255, 255, 255), -1, 8, 0);
+					imshow("QR code old", qr_thres);
+					imshow("image", image);
+					waitKey(0);
+					//#####################
+				}
+			}
+
+			if (success) {
+				//If tag exists retag
+				tag = findTaginList(qrData);
+				if (tag == tagDataMap.size())
+					tagDataMap.push_back(qrData);
+				//int tag = tagnumber++; //<- usually find next free tag - global variable?
+
+				//create QRCode Object
+				QRCode newCode(tag);
+				newCode.pos = QRPos;
+				newCode.decode_success = success;
+				newCode.data = qrData; // <-- we need this or always over tag?
+
+				//if (fipImage.size() == 3) //<- only do this if we had all threeFiPs so its sure that pD and pA are right - not sure if this is the best idea anymore.
+				newCode.orientation = cv_getOrientation(pA, pD);
+
+				//here have a list that gets a new entry with the tag indentification pair
+
+				returnCode = newCode;
+			}
+			else {
+				// TODO : WHAT SHOULD HAPPEN HERE IN MULTI
+				QRCode newCode(0);
+				newCode.pos = QRPos;
+				returnCode = newCode; // <-- will get deleted anywaysbecause tag == 0 
+			}
 		}
 	}
 
@@ -735,13 +842,13 @@ int cv_findCorners(Point& pA, FiP fip_B, FiP fip_C, Point& pD, Point QRPos) {
 		decrementB = 3;
 	}
 	else if (indexOfPointB == 3) {
-		incrementB = -1;
+		incrementB = -3;
 	}
 	if (indexOfPointC == 0) {
 		decrementC = 3;
 	}
 	else if (indexOfPointC == 3) {
-		incrementC = -1;
+		incrementC = -3;
 	}
 	if (pA.x != 0 && pA.y != 0) { //This is stupid TODO check how properly check for null objects
 		Point linePointB;
